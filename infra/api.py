f4a7169becdf37a5565f9184018f85a55e579c8e
Neil Evan Christanto <neilchristanto@gmail.com>
2016-08-28 11:04:40 -0700

    API work
    Change get_csv to get_pd_df, returning pandas dataFrame
    Complete get_pd_df given time range and ticker
    Update test

--- infra/api.py
+++ infra/api.py
@@ -1,8 +1,17 @@
-import time as _time
+from datetime import date
+from datetime import timedelta
+from urllib import request as req
+
 from .errors import api_error
+
+import pandas as pd
 
 """
 Get CSV file from Yahoo
+The URL has the month starting from 0-11
+So the month is offset by -1
+
+Input:
   ticker    : stock symbol
   endDate   : choices:
                 - 'today'
@@ -10,7 +19,38 @@ Get CSV file from Yahoo
   startDate : choices:
                 - days delta (e.g. '-30')
                 - 'YYYY-MM-DD'
+
+Output:
+  pandas dataFrame
 """
-def get_csv(ticker=None, endDate='today', startDate='-30'):
+def get_pd_df(ticker=None, endDate='today', startDate='-30'):
   if ticker == None:
-    raise api_error('get_csv', 'ticker not specified')
+    raise api_error('get_pd_df', 'ticker not specified')
+
+  # determine end period
+  if endDate == 'today':
+    bYear   = date.today().year
+    bMonth  = date.today().month - 1
+    bDate   = date.today().day
+  else:
+    bYear, bMonth, bDate = endDate.split('-')
+
+  # determine start period
+  if startDate[0] == '-':
+    aTemp   = date.today() + timedelta(days=int(startDate))
+    aYear   = aTemp.year
+    aMonth  = aTemp.month - 1
+    aDate   = aTemp.day
+  else:
+    aYear, aMonth, aDate = startDate.split('-')
+  
+  # get CSV from Yahoo
+  url = "http://chart.finance.yahoo.com/table.csv?s={}&a={}&b={}&c={}&d={}&e={}&f={}&g=d&ignore=.csv".\
+    format(ticker.upper(), aMonth, aDate, aYear, bMonth, bDate, bYear)
+ 
+  try:
+    retVal = pd.read_csv(url, index_col='Date', parse_dates=True)
+  except:
+    raise api_error('get_pd_df', 'url {} can\'t be opened'.format(url))
+
+  return retVal
--- tests/api_test.py
+++ tests/api_test.py
@@ -6,18 +6,26 @@ class api_test():
   failCnt = 0
   failTst = []
 
+  ###################################################
   def empty_ticker(self):
     print("...Testing empty ticker") 
     try:
-      api.get_csv()
+      api.get_pd_df()
     except api_error as e:
       if e.msg != 'ticker not specified':
         self.failCnt += 1
         self.failTst.append('empty_ticker')
 
+  ###################################################
+  def ticker_default_period(self, ticker):
+    print("...Testing ticker {} with default period".format(ticker.upper()))
+    csv = api.get_pd_df(ticker)
+    print(csv)
+
   def start_test(self):
     print("Starting api_test")
     self.empty_ticker()
+    self.ticker_default_period('spy')
 
   def summary(self):
     print("Summary:")
